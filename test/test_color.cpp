a3848d7e34e35b97a6a8bce5e0fa6effa64bf972
Malte Vrampe <Athrunen@gmail.com>
2020-01-05 19:31:05 +0100

    Added more tests and streamlined the old ones

--- test/test_color.cpp
+++ test/test_color.cpp
@@ -6,11 +6,41 @@
 void test_rgb2hsv() {
     std::array<double, 3> rgb = {0.78431372549, 0.392156862745, 0.588235294118};
     std::array<double, 3> hsv = color::rgb2hsv(rgb);
-    int h = round(hsv[0]);
-    int s = round(hsv[1] * 100);
-    int v = round(hsv[2] * 100);
     int expected[] = {330, 50, 78};
-    int actual[] = {h, s, v};
+    int actual[] = {(int)round(hsv[0]), (int)round(hsv[1] * 100), (int)round(hsv[2] * 100)};
+    TEST_ASSERT_EQUAL_INT_ARRAY(expected, actual, 3);
+}
+
+void test_hsv2rgb() {
+    std::array<double, 3> hsv = {330, 0.5, 0.78};
+    std::array<double, 3> rgb = color::hsv2rgb(hsv);
+    int expected[] = {78, 39, 59};
+    int actual[] = {(int)round(rgb[0] * 100), (int)round(rgb[1] * 100), (int)round(rgb[2] * 100)};
+    TEST_ASSERT_EQUAL_INT_ARRAY(expected, actual, 3);
+}
+
+void test_rgb2hsv2rgb() {
+    int factor = pow(10, 9);
+    std::array<double, 3> rgb = {0.78431372549, 0.392156862745, 0.588235294118};
+    std::array<double, 3> hsv = color::rgb2hsv(rgb);
+    std::array<double, 3> rgb2 = color::hsv2rgb(hsv);
+    int expected[] = {(int)round(rgb[0] * factor), (int)round(rgb[1] * factor), (int)round(rgb[2] * factor)};
+    int actual[] = {(int)round(rgb2[0] * factor), (int)round(rgb2[1] * factor), (int)round(rgb2[2] * factor)};
+    TEST_ASSERT_EQUAL_INT_ARRAY(expected, actual, 3);
+}
+
+//HSI
+
+void test_rgb2hsi2rgb() {
+    int factor = pow(10, 2);
+    std::array<double, 3> rgb = {0.78431372549, 0.392156862745, 0.588235294118};
+    std::array<double, 3> hsi = color::rgb2hsi(rgb);
+    printf("%f \n", hsi[0]);
+    printf("%f \n", hsi[1]);
+    printf("%f \n", hsi[2]);
+    std::array<double, 3> rgb2 = color::hsi2rgb(hsi);
+    int expected[] = {(int)round(rgb[0] * factor), (int)round(rgb[1] * factor), (int)round(rgb[2] * factor)};
+    int actual[] = {(int)round(rgb2[0] * factor), (int)round(rgb2[1] * factor), (int)round(rgb2[2] * factor)};
     TEST_ASSERT_EQUAL_INT_ARRAY(expected, actual, 3);
 }
 
@@ -18,6 +48,9 @@ void test_rgb2hsv() {
 void process() {
     UNITY_BEGIN();
     RUN_TEST(test_rgb2hsv);
+    RUN_TEST(test_hsv2rgb);
+    RUN_TEST(test_rgb2hsv2rgb);
+    RUN_TEST(test_rgb2hsi2rgb);
     UNITY_END();
 }
 
