bad62171084631f8cb903ae9c81c60e1f87bf104
Malte Vrampe <athrunen@gmail.com>
2020-03-28 21:00:22 +0100

    Clientmode does need no display pins

--- src/config.h
+++ src/config.h
@@ -9,12 +9,17 @@ namespace config
     
     #define FULLMODE
 
+    #ifdef FULLMODE
+
     const byte
         SWITCH_PIN(35),
         UP_PIN(33),
         DOWN_PIN(32);
-    const byte led_pins[4] = {15, 27, 2, 16};
     const int screen_steps = 255;
+    
+    #endif
+
+    const byte led_pins[4] = {15, 27, 2, 16};
     const int resolution = 16;
     const int resolution_factor = pow(2, resolution) - 1;
     const bool debug = true;
